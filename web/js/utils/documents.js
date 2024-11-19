32c27dd7504a4412356e4f7cf4e5f5c4b626ec5e
SMG-web-dev <smanjon2021@gmail.com>
2024-11-19 09:57:38 +0100

    IOS PDFs

--- web/js/utils/documents.js
+++ web/js/utils/documents.js
@@ -1,6 +1,25 @@
 export function setupDocuments() {
     const pdfViewer = document.getElementById('pdf-viewer');
     const buttons = document.querySelectorAll('.document-btn');
+    const pdfContainer = document.querySelector('.pdf-container');
+
+    // Función para detectar iOS
+    function isIOS() {
+        return [
+            'iPad Simulator',
+            'iPhone Simulator',
+            'iPod Simulator',
+            'iPad',
+            'iPhone',
+            'iPod'
+        ].includes(navigator.platform)
+            // iPad on iOS 13 detection
+            || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+    }
+
+    if (isIOS()) {
+        pdfContainer.classList.add('ios');
+    }
 
     if (pdfViewer && buttons.length > 0) {
         // Cargar el PDF almacenado o el primero por defecto
